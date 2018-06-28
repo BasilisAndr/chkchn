e5aea65acfccfc71e104ef8994394b834380a87b
BasilisAndr <blindedbysunshine@gmail.com>
2018-06-28 18:02:57 +0300

    late changes

--- scripts/corpus-stat.sh
+++ scripts/corpus-stat.sh
@@ -7,10 +7,13 @@
 
 
 #CMD="cat corpa/en.crp.txt"
-CMD="cat ../corpora/ckt.crp.txt"
-
-F=../corpora/corpus-stat-res.txt
-
+# CMD="cat ../corpora/ckt.crp.txt"
+#
+# F=../corpora/corpus-stat-res.txt
+for fil in `ls ../corpora/corporatxt`; do
+  echo "$fil";
+  $CMD="cat ../corpora/corporatxt/$fil";
+  $F="/tmp/$fil"
 # Calculate the number of tokenised words in the corpus:
 # for some reason putting the newline in directly doesn't work, so two seds
 $CMD | apertium-destxt | hfst-proc ../ckt.mor.hfstol | apertium-retxt | sed 's/\$[^^]*\^/$^/g' | sed 's/\$\^/$\
@@ -45,12 +48,12 @@ echo "Coverage: $COVERAGE %"
 
 echo "Top unknown words in the corpus:"
 cat $F | grep '\*' | sort -f | uniq -c | sort -gr | head -10
-
+done
 # Update hitparade
-cat $F | cut -f2 -d'^' | cut -f1 -d'/' | sort -f | uniq -c | sort -gr  > ../corpora/ckt.hitparade.txt
-
-# Update glossed hitparade
-bash hitparade.sh > ../corpora/hitparade.txt
-
-# Update unknown list
-cat ../corpora/hitparade.txt | grep '/\*[^0-9]' > ../corpora/unknown.txt
+# cat $F | cut -f2 -d'^' | cut -f1 -d'/' | sort -f | uniq -c | sort -gr  > ../corpora/ckt.hitparade.txt
+#
+# # Update glossed hitparade
+# bash hitparade.sh > ../corpora/hitparade.txt
+#
+# # Update unknown list
+# cat ../corpora/hitparade.txt | grep '/\*[^0-9]' > ../corpora/unknown.txt
