54a8dfe0e8e32a647b99c3615197df1ca9cc4929
BasilisAndr <blindedbysunshine@gmail.com>
2018-07-31 17:41:14 +0300

    fixed new V-IV/V-TV

--- alltest.sh
+++ alltest.sh
@@ -2,10 +2,11 @@
 
 TXT="cat test.lookup.txt"
 F="/tmp/fails.txt"
-$TXT | hfst-lookup ckt.gen.hfstol | grep '>+?' | cut -f1 > $F
+# $TXT | hfst-lookup ckt.lexc.hfst | grep '>+?' | cut -f1 > $F
+$TXT | hfst-lookup ckt.lexc.hfst > "test.lookup.all"
 
-echo "Failed forms:"
-
-for i in `cat $F` ; do
-  echo $i ;
-done
+# echo "Failed forms:"
+#
+# for i in `cat $F` ; do
+#   echo $i ;
+# done
