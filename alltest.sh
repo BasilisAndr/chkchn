022f8c039c54132a5fbc6dec5c8befc74c1cb957
BasilisAndr <blindedbysunshine@gmail.com>
2018-07-31 18:09:19 +0300

    now all non-derived verb forms working

--- alltest.sh
+++ alltest.sh
@@ -3,7 +3,7 @@
 TXT="cat test.lookup.txt"
 F="/tmp/fails.txt"
 # $TXT | hfst-lookup ckt.lexc.hfst | grep '>+?' | cut -f1 > $F
-$TXT | hfst-lookup ckt.lexc.hfst > "test.lookup.all"
+$TXT | hfst-lookup ckt.gen.hfst > "test.lookup.all"
 
 # echo "Failed forms:"
 #
